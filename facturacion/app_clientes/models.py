e01765fab0c3b9459d0bd98d5af4eddb2d33d858
yorugua222 <78772059+yorugua222@users.noreply.github.com>
2023-02-13 22:51:36 -0300

    models

--- facturacion/app_clientes/models.py
+++ facturacion/app_clientes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class cliente(models.Model):
+    nombre=models.CharField(max_length=100)
+    telefono=models.CharField(max_length=15)
+    rut=models.CharField(max_length=12)
+    cedula=models.CharField(max_length=8, unique=True)
+    direccion=models.CharField(max_length=255)
+    email=models.EmailField(max_length=255)
+    fecha_nacimiento=models.DateField()
+    detalle=models.CharField(max_length=255)
--- facturacion/facturacion/settings.py
+++ facturacion/facturacion/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_clientes',
+    'app_comisiones',
+    'app_facturacion',
+    'app_pago',
+    'app_producto',
+    'app_vendedores',
 ]
 
 MIDDLEWARE = [
